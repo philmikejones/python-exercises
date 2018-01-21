f1f163fa211c448068bb50d0d34d2d35d4a6eb38
Phil Mike Jones <philmikejones@gmail.com>
2018-01-21 22:32:55 +0000

    add exercise 13

---
+++ exercise13.py
@@ -0,0 +1,8 @@
+# Exercise 13
+from sys import argv
+script, first, second, third = argv
+
+print(f"The script is called: {script}")
+print(f"The first argument is: {first}")
+print(f"The second argument is: {second}")
+print(f"The third argument is: {third}")
