9b67ce8f84b2ddb021f619136745777147867307
Phil Mike Jones <philmikejones@gmail.com>
2019-09-03 19:36:17 +0100

    add multiline f print

--- prompt.py
+++ prompt.py
@@ -9,11 +9,11 @@ age = input(f"How old are you? ")
 
 confirm = input(f"So your name is {user_name} and you're {age} years old? y/n ")
 
-if confirm is "y":
-    print("Great")
-elif confirm is "n":
+if confirm is "n":
     print("Sorry I got something wrong")
     age = input("So what's your correct age? ")
-    print(f"So your name is {user_name} and your age is {age}.")
-else:
-    print("Please confirm retry")
+
+print(f"""
+So your name is {user_name} and you're {age} years old
+Great
+""")
