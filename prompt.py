85cb0532c6946af33a3968fd60097903a8b5e32f
Phil Mike Jones <philmikejones@gmail.com>
2019-09-03 19:32:23 +0100

    update ex. 14

--- prompt.py
+++ prompt.py
@@ -4,3 +4,16 @@ script, user_name = sys.argv
 prompt = '> '
 
 print(f"Hi {user_name}, I'm {script}")
+
+age = input(f"How old are you? ")
+
+confirm = input(f"So your name is {user_name} and you're {age} years old? y/n ")
+
+if confirm is "y":
+    print("Great")
+elif confirm is "n":
+    print("Sorry I got something wrong")
+    age = input("So what's your correct age? ")
+    print(f"So your name is {user_name} and your age is {age}.")
+else:
+    print("Please confirm retry")
