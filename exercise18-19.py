5a2cf544890488b8f4c85c0fa1f231f587a78967
Phil Mike Jones <philmikejones@gmail.com>
2019-09-11 16:49:45 +0100

    add exercise 19

---
+++ exercise18-19.py
@@ -0,0 +1,13 @@
+def print_two(arg1, arg2):
+  print(f"arg1: {arg1}")
+  print(f"arg2: {arg2}")
+
+print_two("Coffee", "Cake")
+
+# what about using input?
+
+print("enter two numbers you would like to sum")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print(num1 + num2)
--- exercise18.py
+++
@@ -1,5 +0,0 @@
-def print_two(arg1, arg2):
-  print(f"arg1: {arg1}")
-  print(f"arg2: {arg2}")
-
-print_two("Coffee", "Cake")
